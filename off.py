7d8bbeb344f5699fe002e5eb5b1f140f80ceb232
meee418 <dustfranco@gmail.com>
2018-09-06 23:40:46 +0000

    python script to just turn all LEDs off

---
+++ off.py
@@ -0,0 +1,36 @@
+#
+# NeoPixel library strandtest example
+# Author: Tony DiCola (tony@tonydicola.com)
+#
+# Direct port of the Arduino NeoPixel library strandtest example.  Showcases
+# various animations on a strip of NeoPixels.
+
+import time
+from neopixel import *
+import argparse
+from random import randint
+import numpy as np
+
+# LED strip configuration:
+LED_COUNT      = 1185  # Number of LED pixels.
+LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
+#LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
+LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
+LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
+LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
+LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
+
+strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+strip.begin()
+
+def turn_off_all_leds():
+  set_leds_in_range((0,LED_COUNT), Color(0,0,0))
+  strip.show()
+
+def set_leds_in_range(input_range, color):
+  for x in range(input_range[0], input_range[1]):
+    strip.setPixelColor(x, color)
+
+turn_off_all_leds()
+
