8d4380a04506b58558ae1a8a008911b60b92a065
Dustin Franco <dustin@edyn.com>
2018-09-06 17:30:53 -0700

    finished color preset file

---
+++ color_presets.py
@@ -0,0 +1,21 @@
+from neopixel import *
+
+red = Color(255,0,0)
+green = Color(0,255,0)
+blue = Color(0,0,255)
+yellow = Color(255,255,0)
+pink = Color(0,255,255)
+cyan = Color(255,0,255)
+white = Color(255,255,255)
+off = Color(0,0,0)
+
+all_colors = [red, green, blue, yellow, pink, cyan, white]
+
+def multi_color_echo(layers = 5, off_layers = 6):
+  simple_color_preset = []
+  for c in all_colors:
+    for m in range(0, layers):
+      simple_color_preset.append(c)
+    for m in range(0, off_layers):
+      simple_color_preset.append(off)
+  return simple_color_preset
